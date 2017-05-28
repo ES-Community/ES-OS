8ed2aa62f8ff6b2b7f4d284a09697f0b085ada54
fraxken <gentilhomme.thomas@gmail.com>
2017-05-28 16:16:23 +0200

    Update test package

--- packages/test.js
+++ packages/test.js
@@ -3,7 +3,7 @@ const Package = require('../kernel/package.js');
 class Test extends Package {
 
     constructor() {
-        super();
+        super("test");
         this.commands.set('get_info',async (core,pkg) => {
             core.stdout.push('get_info triggered \n');
         });
